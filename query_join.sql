46817951dfda009902a1b5e29eaf9e7209ea2fae
Claudio9616 <clares9611@gmail.com>
2024-03-04 16:02:35 +0100

    query join to fix other 4 problems

---
+++ query_join.sql
@@ -0,0 +1,9 @@
+-- Selezionare tutti gli studenti iscritti al Corso di Laurea in Economia
+SELECT S.`name`, S.`surname`, D.`name` FROM `students` AS S JOIN `degrees`AS D ON S.`degree_id` = D.`id` WHERE D.`name` LIKE '%economia';
+-- Selezionare tutti i Corsi di Laurea del Dipartimento di Neuroscienze
+SELECT D.name, DE.name FROM `degrees` AS D JOIN `departments` AS DE ON D.department_id = DE.id WHERE DE.name LIKE '%neuroscienze';
+-- Selezionare tutti i corsi in cui insegna Fulvio Amato (id=44)
+SELECT C.name AS 'nome corso', T.name AS 'nome professore', T.surname AS 'cognome professore' FROM `teachers` AS T JOIN `course_teacher` AS CT ON T.id = CT.teacher_id JOIN `courses` AS C ON CT.course_id = C.id WHERE T.name = 'fulvio' AND T.surname = 'amato';
+-- Selezionare tutti gli studenti con i dati relativi al corso di laurea a cui sono iscritti e il relativo dipartimento, in ordine alfabetico per cognome e nome
+
+
