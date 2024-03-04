1fee6d68ca7ad14d4c5a1dbaf53589f17e00c7d8
Claudio9616 <clares9611@gmail.com>
2024-03-04 16:43:36 +0100

    dubbio su riga 15

--- query_join.sql
+++ query_join.sql
@@ -3,7 +3,15 @@ SELECT S.`name`, S.`surname`, D.`name` FROM `students` AS S JOIN `degrees`AS D O
 -- Selezionare tutti i Corsi di Laurea del Dipartimento di Neuroscienze
 SELECT D.name, DE.name FROM `degrees` AS D JOIN `departments` AS DE ON D.department_id = DE.id WHERE DE.name LIKE '%neuroscienze';
 -- Selezionare tutti i corsi in cui insegna Fulvio Amato (id=44)
-SELECT C.name AS 'nome corso', T.name AS 'nome professore', T.surname AS 'cognome professore' FROM `teachers` AS T JOIN `course_teacher` AS CT ON T.id = CT.teacher_id JOIN `courses` AS C ON CT.course_id = C.id WHERE T.name = 'fulvio' AND T.surname = 'amato';
+SELECT T.id AS 'codice professore', C.name AS 'nome corso', T.name AS 'nome professore', T.surname AS 'cognome professore' FROM `teachers` AS T JOIN `course_teacher` AS CT ON T.id = CT.teacher_id JOIN `courses` AS C ON CT.course_id = C.id WHERE T.name = 'fulvio' AND T.surname = 'amato';
 -- Selezionare tutti gli studenti con i dati relativi al corso di laurea a cui sono iscritti e il relativo dipartimento, in ordine alfabetico per cognome e nome
+SELECT D.name, D.level, S.name, S.surname, DE.name
+FROM `students` AS S 
+JOIN `degrees`AS D 
+ON S.degree_id = D.id
+JOIN `departments`AS DE 
+ON D.department_id = DE.id
+ORDER BY S.name ASC,
+ORDER BY `students`.surname ASC;
 
 
